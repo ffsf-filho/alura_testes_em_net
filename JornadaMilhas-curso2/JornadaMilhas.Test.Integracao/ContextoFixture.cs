039910144617dd31745bf5affe7d7dd7db71d3f1
franciscof <franciscof@cetsp.com.br>
2024-12-11 00:34:02 -0300

    Implementa a aula de docker, esta falhando os testes

--- JornadaMilhas-curso2/JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas-curso2/JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -1,23 +1,39 @@
 ﻿using JornadaMilhas.Dados;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
 
-public class ContextoFixture
+public class ContextoFixture : IAsyncLifetime
 {
-    public JornadaMilhasContext Context { get;}
+    public JornadaMilhasContext Context { get; private set; }
+    private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+        .WithImage("mcr-microsoft.com/mssql/server:2022-latest")
+        //.WithPortBinding(57689, true)
+        //.WithCleanUp(false)
+        .Build();
 
     public ContextoFixture()
     {
-        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
-            .Options;
+        //var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+        //    .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+        //    .Options;
 
+        //Context = new JornadaMilhasContext(options);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _msSqlContainer.StartAsync();
+        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+            .UseSqlServer(_msSqlContainer.GetConnectionString())
+            .Options;
         Context = new JornadaMilhasContext(options);
+        Context.Database.Migrate();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _msSqlContainer.StopAsync();
     }
 }
