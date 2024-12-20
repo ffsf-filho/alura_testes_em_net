d3e49bd84d73f28744968eab9d5d89a401d8c0cf
franciscof <franciscof@cetsp.com.br>
2024-12-19 23:08:56 -0300

    Implementa a aula de consultas

--- JornadaMilhas-API/test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
+++ JornadaMilhas-API/test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -1,5 +1,7 @@
 ﻿using JornadaMilhas.Dominio.Entidades;
 using JornadaMilhas.Dominio.ValueObjects;
+using JornadaMilhas.Integration.Test.API.DataBuilders;
+using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Json;
 
 namespace JornadaMilhas.Integration.Test.API;
@@ -42,5 +44,102 @@ public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory
         Assert.Equal(ofertaExistente.Preco, response.Preco, 0.001);
         Assert.Equal(ofertaExistente.Rota.Origem, response.Rota.Origem);
         Assert.Equal(ofertaExistente.Rota.Destino, response.Rota.Destino);
+    }
+
+    [Fact]
+    public async Task Recuperar_OfertasViagem_Na_Consulta_Paginada()
+    {
+        //arrange
+        var ofertaDataBuilder = new OfertaViagemDataBuilder();
+        var listaDeOfertas = ofertaDataBuilder.Generate(80);
+        _app.Context.OfertasViagem.AddRange(listaDeOfertas);
+        _app.Context.SaveChanges();
+
+        using var client = await _app.GetClientWithAccessTokenAsync();
+
+        int pagina = 1;
+        int tamanhoPorPagina = 80;
+
+        //act
+        var response = await client.GetFromJsonAsync<ICollection<OfertaViagem>>($"/ofertas-viagem?pagina={pagina}&tamanhoPorPagina={tamanhoPorPagina}");
+
+        //assert
+        Assert.NotNull(response);
+        Assert.Equal(tamanhoPorPagina, response.Count);
+    }
+
+    [Fact]
+    public async Task Recuperar_OfertasViagem_Na_Consulta_Ultima_Pagina()
+    {
+        //arrange
+        _app.Context.Database.ExecuteSqlRaw("Delete from OfertasViagem");
+
+        var ofertaDataBuilder = new OfertaViagemDataBuilder();
+        var listaDeOfertas = ofertaDataBuilder.Generate(80);
+        _app.Context.OfertasViagem.AddRange(listaDeOfertas);
+        _app.Context.SaveChanges();
+
+        using var client = await _app.GetClientWithAccessTokenAsync();
+
+        int pagina = 4;
+        int tamanhoPorPagina = 25;
+
+        //act
+        var response = await client.GetFromJsonAsync<ICollection<OfertaViagem>>($"/ofertas-viagem?pagina={pagina}&tamanhoPorPagina={tamanhoPorPagina}");
+
+        //assert
+        Assert.NotNull(response);
+        Assert.Equal(5, response.Count);
+    }
+
+    [Fact]
+    public async Task Recuperar_OfertasViagem_Na_Consulta_Com_Pagina_Inexistente()
+    {
+        //arrange
+        _app.Context.Database.ExecuteSqlRaw("Delete from OfertasViagem");
+
+        var ofertaDataBuilder = new OfertaViagemDataBuilder();
+        var listaDeOfertas = ofertaDataBuilder.Generate(80);
+        _app.Context.OfertasViagem.AddRange(listaDeOfertas);
+        _app.Context.SaveChanges();
+
+        using var client = await _app.GetClientWithAccessTokenAsync();
+
+        int pagina = 5;
+        int tamanhoPorPagina = 25;
+
+        //act
+        var response = await client.GetFromJsonAsync<ICollection<OfertaViagem>>($"/ofertas-viagem?pagina={pagina}&tamanhoPorPagina={tamanhoPorPagina}");
+
+        //assert
+        Assert.NotNull(response);
+        Assert.Equal(0, response.Count);
+    }
+
+    [Fact]
+    public async Task Recuperar_OfertasViagem_Na_Consulta_Com_Pagina_Com_Valor_Negativo()
+    {
+        //arrange
+        _app.Context.Database.ExecuteSqlRaw("Delete from OfertasViagem");
+
+        var ofertaDataBuilder = new OfertaViagemDataBuilder();
+        var listaDeOfertas = ofertaDataBuilder.Generate(80);
+        _app.Context.OfertasViagem.AddRange(listaDeOfertas);
+        _app.Context.SaveChanges();
+
+        using var client = await _app.GetClientWithAccessTokenAsync();
+
+        int pagina = -5;
+        int tamanhoPorPagina = 25;
+
+        //act + assert
+        await Assert.ThrowsAsync<HttpRequestException>(async () =>
+        {
+            var response = await client.GetFromJsonAsync<ICollection<OfertaViagem>>($"/ofertas-viagem?pagina={pagina}&tamanhoPorPagina={tamanhoPorPagina}");
+        });
+
+        //assert
+        //Assert.NotNull(response);
+        //Assert.Equal(0, response.Count);
     }
 }
