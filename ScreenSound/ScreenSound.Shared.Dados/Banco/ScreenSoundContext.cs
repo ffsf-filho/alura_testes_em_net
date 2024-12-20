aef77850eadff541a39b73073b15ace1a60d56ea
franciscof <franciscof@cetsp.com.br>
2024-12-19 23:53:51 -0300

    Inclui a API ScreenSound que será usada na aula de desafio

---
+++ ScreenSound/ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
+    //private string connectionString = "Data Source=localhost,1438;Initial Catalog=ScreenSoundV0;User Id=sa;Password=ssExpress1823;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Server=localhost,1438;Database=ScreenSoundV0;User Id=sa;Password=ssExpress1823;Encrypt=False;TrustServerCertificate=true;MultipleActiveResultSets=true;";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Musica>()
+            .HasMany(c => c.Generos)
+            .WithMany(c => c.Musicas);
+    }
+
+}
