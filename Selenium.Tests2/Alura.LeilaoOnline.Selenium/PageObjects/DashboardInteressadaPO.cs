8e07cd39055111c192991a1fcbf1a55777d381fa
franciscof <franciscof@cetsp.com.br>
2024-12-31 01:57:56 -0300

    Implementa a aula Elementos invisíveis, porém está apresentando erro no logout

---
+++ Selenium.Tests2/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class DashboardInteressadaPO
+    {
+        private IWebDriver driver;
+        private By byLogoutLink;
+        private By byMeuPerfilLink;
+
+        public DashboardInteressadaPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.byLogoutLink = By.Id("logout");
+            this.byMeuPerfilLink = By.Id("meu-perfil");
+        }
+
+        internal void EfetuarLogout()
+        {
+            var linkMeuPerfil = driver.FindElement(byMeuPerfilLink);
+            var linkLogout = driver.FindElement(byLogoutLink);
+
+            IAction acaoLogout = new Actions(this.driver)
+                //Mover para o elemento meu-perfil
+                .MoveToElement(linkMeuPerfil)
+                //mover para o link de logout
+                .MoveToElement(linkLogout)
+                //clicar no link de logout
+                .Click()
+                .Build();
+            acaoLogout.Perform();
+        }
+    }
+}
---
+++ Selenium.Tests2/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarLogout.cs
@@ -0,0 +1,36 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarLogout(TestFixture fixture)
+        {
+            this.driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginValidoDeveIrParaHomeNaoLogada()
+        {
+            //arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+            
+            var dashboardPO = new DashboardInteressadaPO(driver);
+
+            //act
+            dashboardPO.EfetuarLogout();
+
+            //assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
