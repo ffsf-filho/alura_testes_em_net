b035687f7af693ccb08b3399afc7f660640eb339
franciscof <franciscof@cetsp.com.br>
2024-12-31 23:00:04 -0300

    Implementa a aula Frameworks que dificultam

---
+++ Selenium.Tests2/Alura.LeilaoOnline.Selenium/Helpers/SelectMaterialize.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Selenium.Helpers
+{
+    public class SelectMaterialize
+    {
+        private IWebDriver driver;
+        private IWebElement selectWrapper;
+        private IEnumerable<IWebElement> opcoes;
+
+        public SelectMaterialize(IWebDriver driver, By locatorSelectWrapper)
+        {
+            this.driver = driver;
+            this.selectWrapper = driver.FindElement(locatorSelectWrapper);
+            opcoes = selectWrapper.FindElements(By.CssSelector("li>span"));
+        }
+
+        public IEnumerable<IWebElement> Options => opcoes;
+
+        private void OpenWrapper()
+        {
+            selectWrapper.Click();
+        }
+
+        private void LoseFocus()
+        {
+            selectWrapper
+                .FindElement(By.TagName("li"))
+                .SendKeys(Keys.Tab);
+        }
+
+        public void DeselectAll()
+        {
+            OpenWrapper();
+
+            opcoes.ToList().ForEach(o =>
+            {
+                o.Click();
+            });
+
+            LoseFocus();
+        }
+
+        public void SelectByText(string option)
+        {
+            OpenWrapper();
+
+            opcoes
+                .Where(o => o.Text.Contains(option))
+                .ToList()
+                .ForEach(o =>
+                {
+                    o.Click();
+                });
+
+            LoseFocus();
+        }
+    }
+}
--- Selenium.Tests2/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
+++ Selenium.Tests2/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -1,5 +1,9 @@
-﻿using OpenQA.Selenium;
+﻿using Alura.LeilaoOnline.Selenium.Helpers;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Alura.LeilaoOnline.Selenium.PageObjects
 {
@@ -8,15 +12,42 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         private IWebDriver driver;
         private By byLogoutLink;
         private By byMeuPerfilLink;
+        private By bySelectCategorias;
+        private By byInputTermo;
+        private By byInputAndamento;
+        private By byBotaoPesquisar;
 
         public DashboardInteressadaPO(IWebDriver driver)
         {
             this.driver = driver;
             this.byLogoutLink = By.Id("logout");
             this.byMeuPerfilLink = By.Id("meu-perfil");
+            this.bySelectCategorias = By.ClassName("select-wrapper");
+            this.byInputTermo = By.Id("termo");
+            this.byInputAndamento = By.ClassName("switch");
+            this.byBotaoPesquisar = By.CssSelector("form>button.btn");
         }
 
-        internal void EfetuarLogout()
+        public void PesquisarLeiloes(List<string> categorias, string termo, bool emAndamento)
+        {
+            var select = new SelectMaterialize(driver, bySelectCategorias);
+            select.DeselectAll();
+
+            categorias.ForEach(categ => 
+            {
+                select.SelectByText(categ);
+            });
+
+            driver.FindElement(byInputTermo).SendKeys(termo);
+
+            if (emAndamento)
+            {
+                driver.FindElement(byInputAndamento).Click();
+            }
+            driver.FindElement(byBotaoPesquisar).Click();
+        }
+
+        public void EfetuarLogout()
         {
             var linkMeuPerfil = driver.FindElement(byMeuPerfilLink);
             var linkLogout = driver.FindElement(byLogoutLink);
