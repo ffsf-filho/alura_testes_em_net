4f639e059dbd4b50cc57ee2d877f398cd388afb5
franciscof <franciscof@cetsp.com.br>
2025-01-04 02:59:38 -0300

    Implementa a aula de Outros recursos

---
+++ Selenium.Tests3/Alura.LeilaoOnline.Selenium/PageObjects/HomeNaoLogadaPO.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class HomeNaoLogadaPO
+    {
+        private IWebDriver driver;
+        public MenuNaoLogadoPO Menu { get; set; }
+
+        public HomeNaoLogadaPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.Menu = new MenuNaoLogadoPO(driver);
+        }
+
+        public void Visitar()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000");
+        }
+    }
+}
---
+++ Selenium.Tests3/Alura.LeilaoOnline.Selenium/Testes/AoNavegarParaHomeMobile.cs
@@ -0,0 +1,63 @@
+﻿using Alura.LeilaoOnline.Selenium.Helpers;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chromium;
+using System;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    public class AoNavegarParaHomeMobile : IDisposable
+    {
+        private ChromeDriver driver;
+
+        public AoNavegarParaHomeMobile()
+        {
+        }
+
+        [Fact]
+        public void DadaLargura992DeveMostrarMenuMobile()
+        {
+            //arrange
+            ConfiguraLarguraDaTelaMobile(992);
+            var homePO = new HomeNaoLogadaPO(driver);
+
+            //act
+            homePO.Visitar();
+
+            //asert
+            Assert.True(homePO.Menu.MenuMobileVisivel);
+        }
+
+        [Fact]
+        public void DadaLargura993NaoDeveMostrarMenuMobile()
+        {
+            //arrange
+            ConfiguraLarguraDaTelaMobile(993);
+            var homePO = new HomeNaoLogadaPO(driver);
+
+            //act
+            homePO.Visitar();
+
+            //asert
+            Assert.False(homePO.Menu.MenuMobileVisivel);
+        }
+
+        private void ConfiguraLarguraDaTelaMobile(long larguraDaTela)
+        {
+            var deviceSettings = new ChromiumMobileEmulationDeviceSettings();
+            deviceSettings.Width = larguraDaTela;
+            deviceSettings.Height = 800;
+            deviceSettings.UserAgent = "Customizada";
+
+            var options = new ChromeOptions();
+            options.EnableMobileEmulation(deviceSettings);
+            this.driver = new ChromeDriver(TestHelper.PastaDoExecutavel, options);
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
